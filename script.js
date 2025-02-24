56765d10591a9c19778c5fe0b5ea8d94eb55c2db
simoneburrai <burrai.s98@gmail.com>
2025-02-24 18:29:21 +0100

    feat: NaN error input fixed

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
     // Dichiarazione e inizializzazione Variabili
     let kilometers = parseFloat(prompt("Inserire il numero di chilometri da percorrere"));
+    if (isNaN(kilometers)){
+        alert("Inserire un valore numerico");
+        exit();
+    }
     const age = parseFloat(prompt("Inserire la propria età"));
+    if (isNaN(age)){
+        alert("Inserire un valore numerico");
+        exit();
+    }
     const pricePerKilometer = 0.21;
 
     let ticketPrice = kilometers * pricePerKilometer;
@@ -8,16 +16,16 @@
     // Condizioni per lo sconto 
     if (age < 18) {
         ticketPrice = ticketPrice - ((20 * ticketPrice)/ 100);
-        console.log("Avete ottenuto lo sconto del 20%");
+        alert("Avete ottenuto lo sconto del 20%");
     } else if (age >= 65) {
         ticketPrice = ticketPrice - ((40 * ticketPrice)/ 100);
-        console.log("Avete ottenuto lo sconto del 40%");
+        alert("Avete ottenuto lo sconto del 40%");
     }
 
     // Arrotondamento a 2 cifre decimali del prezzo del biglietto 
     ticketPrice = ticketPrice.toFixed(2);
     // Stampa dei dati acquistiti (Chilometri da percorrere e età del passeggero)
     // Stampa del prezzo effettivo del biglietto 
-    console.log(`I chilometri che dovrete percorrere sono ${kilometers}
+    alert(`I chilometri che dovrete percorrere sono ${kilometers}
         La vostra età è di ${age} anni
         Il prezzo del vostro biglietto è di ${ticketPrice} €`);
